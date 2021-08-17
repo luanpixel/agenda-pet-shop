f9afc659314465c385e71be3ce994a8511c2fdde
LuanSSilva <52720974+luanpixel@users.noreply.github.com>
2021-08-17 07:31:44 -0300

    add data

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,9 +2,11 @@ const conexao = require('../insfraestrutura/conexao')
 
 class Atendimento {
     adiciona(atendimento) {
+        const dataCriacao = new Date()
+        const atendimentoDatado = { ...atendimento, dataCriacao }
         const sql = 'INSERT INTO Atendimentos SET ?'
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if (erro) {
                 console.log(erro)
             } else {
